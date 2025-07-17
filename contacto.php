c896323ae4e5ec57f832e55914c0999f8fbb31a3
Jose Alfredo Hernandez Marin <alfredomarin.92@gmail.com>
2025-07-17 12:37:13 -0700

    Update contacto.php

--- contacto.php
+++ contacto.php
@@ -1,1 +1,13 @@
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
 
+$to = "alfredomarin.92@gmail.com";
+$subject = "Nuevo mensaje desde tu blog";
+$headers = "From: $email";
+
+mail($to, $subject, "Nombre: $nombre\nCorreo: $email\n\nMensaje:\n$mensaje", $headers);
+
+echo "Gracias por tu mensaje, $nombre.";
+?>
